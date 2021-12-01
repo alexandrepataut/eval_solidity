520967aa7935b5f2e01e53eec34a1fe36f975bbe
alex4ptt <ale.pataut@live.fr>
2021-12-01 15:51:11 +0100

    withdraw à résoudre => augmente solde bank

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,16 +2,15 @@
 pragma solidity ^0.5.16;
 
 contract Bank {
-    address payable[] public clients;
     mapping(address => uint256) balances;
 
     function deposit() public payable {
-        balances[msg.sender] += msg.value;
+        balances[msg.sender] = balances[msg.sender] + msg.value;
     }
 
     function withdraw() public payable {
         require(balances[msg.sender] >= msg.value);
-        balances[msg.sender] -= msg.value;
+        balances[msg.sender] = balances[msg.sender] - msg.value;
     }
 
     function bankBalance() public view returns(uint256) {
--- main.js
+++ main.js
@@ -1,29 +1,8 @@
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 const ethereumButton = document.querySelector('.enableEthereumButton');
 const showAccount = document.querySelector('.showAccount');
-const contract_address = '0xcf59743a134C832556f7B40d14675e3843574B26';
+const contract_address = '0x508139b2f1D75b300a427342B7b3662E4a643fE8';
 const abi = [
-  {
-    "constant": true,
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "name": "clients",
-    "outputs": [
-      {
-        "internalType": "address payable",
-        "name": "",
-        "type": "address"
-      }
-    ],
-    "payable": false,
-    "stateMutability": "view",
-    "type": "function"
-  },
   {
     "constant": false,
     "inputs": [],
@@ -78,21 +57,19 @@ var Contract = ContractInstance.at(contract_address);
 
 
 function deposit() {
-  document.getElementById("deposit").innerHTML = Contract.deposit({from: web3.eth.accounts[0], value: 1*10**18});
+  Contract.deposit({from: web3.eth.accounts[0], value: 1*10**18});
+  console.log(Contract.balances[web3.eth.accounts[0]]);
 }
 
 function withdraw() {
-  document.getElementById("withdraw").innerHTML = Contract.withdraw({from: web3.eth.accounts[0], value: 1*10**18});
+  Contract.withdraw({from: web3.eth.accounts[0], value: 1*10**18});
 }
 
 function bankBalance() {
   console.log(Contract.bankBalance());
-  document.getElementById("bankBalance").innerHTML = Contract.methods.bankBalance();
+  document.getElementById("bankBalance").innerHTML = Contract.bankBalance();
 }
 
 function balance() {
-  for(i =0; i<Contract.clients().length; i++) {
-    document.write(Contract.clients({from: web3.eth.accounts[i]})+" "+Contract.balance({from: web3.eth.accounts[i]}));
-  }
-  //document.getElementById("bankBalance").innerHTML = Contract.bankBalance();
+  document.getElementById("balance").innerHTML = Contract.balance();
 }
